5e7e7f75cb72e62d5c2cb4d76af64b2758e71384
M6rc0sp <paulo.marcos0108@live.com>
2019-09-13 09:58:07 -0300

    fixing an error about a model

--- models/email.js
+++ models/email.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const config = require('../config');
 
-const userSchema = new mongoose.Schema({
-	user: {
+const emailSchema = new mongoose.Schema({
+	email: {
 		type: String,
     	required: true,
 	},
-	password:{
-		type: String,
+	permission:{
+		type: Boolean,
 		required: true,
 	}
 });
 
 mongoose.connect(`mongodb://heroku_8g99fd99:ja612kvpqutitn96tup42ic955@ds217438.mlab.com:17438/${config.database}`, { useNewUrlParser: true });
-const user = mongoose.model('user', userSchema);
+const email = mongoose.model('email', emailSchema);
 
-module.exports = user;
+module.exports = email;
