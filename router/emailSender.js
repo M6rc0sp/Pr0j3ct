c43abe8199f346f875ab2d705dbbb9499d42c17a
M6rc0sp <paulo.marcos0108@live.com>
2019-09-16 11:49:30 -0300

    google mail

--- router/emailSender.js
+++ router/emailSender.js
@@ -9,27 +9,30 @@ router.post('/emailsender', async (req, res) => {
 
         // create reusable transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
-            host: "smtp.live.com",
+            host: "smtp.gmail.com",
             port: 587,
             secure: false, // upgrade later with STARTTLS
             auth: {
-              user: "paulo.marcos0108@live.com",
-              pass: "70248084461.MP"
+              user: "paulo.marcos0108@gmail.com",
+              pass: "70248084461MP"
             },
             tls: {
                 ignoreTLS: true,
                 rejectUnauthorized: false
               }
           });
-    
-        let info = await transporter.sendMail({
-            from: '"Marcos Paulo 👻" <paulo.marcos0108@live.com>', // sender address
-            to: req.body.email, // list of receivers
-            subject: 'Hello ✔', // Subject line
-            text: req.body.message, // plain text body
-            html: '<b>'+req.body.message+'</b>' // html body
-        });
-        console.log('Message sent: %s', info.messageId);
+          try {
+            let info = await transporter.sendMail({
+              from: '"Marcos Paulo 👻" <paulo.marcos0108@live.com>', // sender address
+              to: req.body.email, // list of receivers
+              subject: 'Hello ✔', // Subject line
+              text: req.body.message, // plain text body
+              html: '<b>'+req.body.message+'</b>' // html body
+          });
+            console.log('Message sent: %s', info.messageId); 
+          } catch (error) {
+            console.log('Error'); 
+          }
     
   });
 
--- router/newemail.js
+++ router/newemail.js
@@ -1,35 +1,34 @@
-'use strict';
-
 const nodemailer = require('nodemailer');
-var express = require('express');
-var router = express.Router();
 
 async function email() {
 
         // create reusable transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
-            host: "smtp.live.com",
+            host: "smtp.gmail.com",
             port: 587,
             secure: false, // upgrade later with STARTTLS
             auth: {
-              user: "paulo.marcos0108@live.com",
-              pass: "70248084461.MP"
+              user: "paulo.marcos0108@gmail.com",
+              pass: "70248084461MP"
             },
             tls: {
                 ignoreTLS: true,
                 rejectUnauthorized: false
               }
           });
-    
-        let info = await transporter.sendMail({
-            from: '"Marcos Paulo 👻" <paulo.marcos0108@live.com>', // sender address
-            to: 'paulo.marcos0108@live.com', // list of receivers
-            subject: 'Hello ✔', // Subject line
-            text: 'req.body.message', // plain text body
-            html: '<b>Hello world?</b>' // html body
-        });
-        console.log('Message sent: %s', info.messageId);
+          try {
+            let info = await transporter.sendMail({
+              from: '"Marcos Paulo 👻" <paulo.marcos0108@live.com>', // sender address
+              to: 'paulo.marcos0108@live.com', // list of receivers
+              subject: 'Hello ✔', // Subject line
+              text: 'req.body.message', // plain text body
+              html: '<b>Hello world?</b>' // html body
+          });
+            console.log('Message sent: %s', info.messageId); 
+          } catch (error) {
+            console.log('Error'); 
+          }
     
   }
 
-email().catch(console.error);
+email();
