b1f021a6023e0e711e6accd23c53ddf0bf258340
M6rc0sp <paulo.marcos0108@live.com>
2019-09-23 12:05:12 -0300

    new layout

--- pr0ject/src/main.js
+++ pr0ject/src/main.js
@@ -2,7 +2,10 @@
 import React, {Component} from "react";
 import axios from 'axios';
 import Linkify from 'react-linkify';
+import ReadMore from 'read-more-react';
 import Abstract from './components/abstract/abstract';
+import './index.css'
+import Button from "react-bootstrap/Button";
 
 class Main extends Component {
 
@@ -10,7 +13,8 @@ class Main extends Component {
 		super(props);
 		this.state = {
     list: [],
-    link: ''
+    link: '',
+    expanded: false
 		};
 	}
 
@@ -20,16 +24,16 @@ class Main extends Component {
             let data = [];
 
             for (var i in res.data.site) {
-                data.push({ titulo: res.data.site[i].titulo, texto: res.data.site[i].texto, img: res.data.site[i].img })
+                data.push({ titulo: res.data.site[i].titulo, texto: res.data.site[i].texto, img: res.data.site[i].img, expanded: false })
             }
 
             this.setState({ list: data });
             console.log("main",this.state.list);
-            if(this.state.list[2]){
-              var l = this.state.list[2].img.split("\\");
-              var li = './'+l[1]+'/'+l[2]+'/'+l[3]+'/'+l[4]+'';
-              this.setState({link: li})
-            }
+            //if(this.state.list[0]){
+            //  var l = this.state.list[0].img.split("\\");
+            //  var li = l[4];
+            //  this.setState({link: li})
+           // }
             console.log(this.state)
           });
 	}
@@ -37,6 +41,18 @@ class Main extends Component {
 	componentWillMount() {
 		this.callAPI();
   }
+
+  onclick=e=>{
+    let id = e.target.id;
+    const newList = this.state.list.slice();
+    if(this.state.list[id].expanded){
+      newList[id].expanded = false; 
+    }else{
+      newList[id].expanded = true; 
+    }
+    this.setState({ list: newList });
+  }
+
   render (){
     return(
       <section className="bg-primary3" id="about">
@@ -44,44 +60,56 @@ class Main extends Component {
           <br/>
           <Abstract/>
           <br/>
-          {//<img src={require(this.state.link)} alt={"img"}/><br/>
-          }
+          <div id="main" className="row" >
         {
-          this.state.list.map(function (list, index) {
+          this.state.list.map((list, index) => {
             if(index===0){                
               return (
-                <div key={index} id="main" className="row" >
-                  <h1 className="text-center">{list.titulo}</h1>
-                  <div  className="col-lg-12 mx-auto text-justify">
-                    <div id="dimg">
-                      <img src={require('./public/img/0.jpg')} alt={"img"+index} /><br/>
-                    </div>
+                  <div  className="col-lg-6 float-left text-justify">
+                    <h3 className="text-center">{list.titulo}</h3>
                     <br/>
-                    <Linkify properties={{target:"_blank", rel:"noopener noreferrer"}}>
-                      <p style={{whiteSpace: 'pre-wrap'}}>{list.texto}</p>
-                    </Linkify>
+                    {list.expanded
+                    ? <div>
+                        <div id="dimg">
+                          <img src={require('./public/img/0.jpg')} alt={"img"+index} /><br/>
+                        </div>
+                        <Linkify properties={{target:"_blank", rel:"noopener noreferrer"}}>
+                          <p style={{whiteSpace: 'pre-wrap'}}>{list.texto}</p>
+                        </Linkify>
+                        <Button variant='outline-dark' id={index} onClick={this.onclick}>recolher</Button>
+                      </div>
+                    : <div>
+                        <ReadMore text={list.texto} readMoreText={'...'}/>
+                        <Button variant='outline-dark' id={index} onClick={this.onclick}>leia mais</Button>
+                      </div>}
                   </div>
-                </div>
               );
             }
             return (
-              <div key={index} id="main" className="row" >
-                  <h1 className="text-center">{list.titulo}</h1>
-                  <div  className="col-lg-12 mx-auto text-justify">
+                  <div  className="col-lg-6 float-left text-justify">
+                  <h3 className="text-center">{list.titulo}</h3>
                     <div id="dimg">
                       {list.img==='abc'
                         ? <div></div>
                         : <img src={list.img} alt={'img'+index}/>}
                     </div>
                     <br/>
-                    <Linkify properties={{target:"_blank", rel:"noopener noreferrer"}}>
-                      <p style={{whiteSpace: 'pre-wrap'}}>{list.texto}</p>
-                    </Linkify>
-                  </div>
-                </div>
+                    {list.expanded
+                    ? <div>
+                        <Linkify properties={{target:"_blank", rel:"noopener noreferrer"}}>
+                          <p key={index+1} style={{whiteSpace: 'pre-wrap'}}>{list.texto}</p>
+                        </Linkify>
+                        <Button variant='outline-dark' key={index+2} id={index} onClick={this.onclick}>recolher</Button>
+                      </div>
+                    : <div>
+                        <ReadMore text={list.texto} key={index+3} readMoreText={'...'} max={1000}/>
+                        <Button variant='outline-dark' key={index+4} id={index} onClick={this.onclick}>leia mais</Button>
+                      </div>}
+                      </div>
             );
           })
         }
+        </div>
         </div>
       </section>
       );
